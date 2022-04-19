c251f5308d2c36325ebf0e0407e59b2397197221
KashbullinRail <kashbullinrail@gmail.com>
2022-04-19 17:58:18 +0300

    Мое домашнее задание № 2 по разделу "Условный оператор"

--- src/ExampleTwo.java
+++ src/ExampleTwo.java
@@ -1,74 +1,80 @@
 /**
  * SkyPro. My code dz#2
  *
- * @author Р Р°РёР»СЊ
+ * @author Раиль
  * @version dated Apr 16, 2022
  */
 public class ExampleTwo {
     public static void main(String[] args) {
 
-        // Р—Р°РґР°РЅРёРµ 1
+        System.out.println();
+        // Задание 1
+        System.out.println("Задание 1");
         int clientOS = 1;
         if (clientOS == 1) {
-            System.out.println("\nРЈСЃС‚Р°РЅРѕРІРёС‚Рµ РІРµСЂСЃРёСЋ РїСЂРёР»РѕР¶РµРЅРёСЏ РґР»СЏ iOS РїРѕ СЃСЃС‹Р»РєРµ");
-        } else System.out.println("РЈСЃС‚Р°РЅРѕРІРёС‚Рµ РІРµСЂСЃРёСЋ РїСЂРёР»РѕР¶РµРЅРёСЏ РґР»СЏ Android РїРѕ СЃСЃС‹Р»РєРµ");
-        System.out.println("\n");
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else System.out.println("Установите версию приложения для Android по ссылке");
+        System.out.println("");
 
-        // Р—Р°РґР°РЅРёРµ 2
+        // Задание 2
+        System.out.println("Задание 2");
         int clientDeviceYear = 2014;
         if (clientOS == 1) {
             if (clientDeviceYear < 2015) {
-                System.out.println("РЈСЃС‚Р°РЅРѕРІРёС‚Рµ РѕР±Р»РµРіС‡РµРЅРЅСѓСЋ РІРµСЂСЃРёСЋ РїСЂРёР»РѕР¶РµРЅРёСЏ РґР»СЏ iOS РїРѕ СЃСЃС‹Р»РєРµ");
-            } else System.out.println("РЈСЃС‚Р°РЅРѕРІРёС‚Рµ РІРµСЂСЃРёСЋ РїСЂРёР»РѕР¶РµРЅРёСЏ РґР»СЏ iOS РїРѕ СЃСЃС‹Р»РєРµ");
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else System.out.println("Установите версию приложения для iOS по ссылке");
         } else
             if (clientDeviceYear < 2015) {
-                System.out.println("РЈСЃС‚Р°РЅРѕРІРёС‚Рµ РѕР±Р»РµРіС‡РµРЅРЅСѓСЋ РІРµСЂСЃРёСЋ РїСЂРёР»РѕР¶РµРЅРёСЏ РґР»СЏ Android РїРѕ СЃСЃС‹Р»РєРµ");
-            } else System.out.println("РЈСЃС‚Р°РЅРѕРІРёС‚Рµ РІРµСЂСЃРёСЋ РїСЂРёР»РѕР¶РµРЅРёСЏ РґР»СЏ Android РїРѕ СЃСЃС‹Р»РєРµ");
-        System.out.println("\n");
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else System.out.println("Установите версию приложения для Android по ссылке");
+        System.out.println("");
 
-        // Р—Р°РґР°РЅРёРµ 3
+        // Задание 3
+        System.out.println("Задание 3");
         int year = 123456;
         if (year % 4 == 0 & (year % 100 != 0 || year % 400 == 0)) {
-                System.out.println(year + " РіРѕРґ СЏРІР»СЏРµС‚СЃСЏ РІРёСЃРѕРєРѕСЃРЅС‹Рј");
-        } else System.out.println(year + " РіРѕРґ РЅРµ СЏРІР»СЏРµС‚СЃСЏ РІРёСЃРѕРєРѕСЃРЅС‹Рј");
-        System.out.println("\n");
+                System.out.println(year + " год является високосным");
+        } else System.out.println(year + " негод не является високосным");
+        System.out.println("");
 
-        // Р—Р°РґР°РЅРёРµ 4
+        // Задание 4
+        System.out.println("Задание 4");
         int deliveryDistance = 60;
         if (deliveryDistance <= 100) {
             if (deliveryDistance <= 60 ) {
                 if (deliveryDistance <= 20 ) {
-                    System.out.println("РџРѕС‚СЂРµР±СѓРµС‚СЃСЏ РґРЅРµР№: 1");
-                } else System.out.println("РџРѕС‚СЂРµР±СѓРµС‚СЃСЏ РґРЅРµР№: 2");
-            } else System.out.println("РџРѕС‚СЂРµР±СѓРµС‚СЃСЏ РґРЅРµР№: 3");
-        } else System.out.println("Р”РѕСЃС‚Р°РІРєР° РЅРµ РїСЂРѕРёР·РІРѕРґРёС‚СЃСЏ");
-        System.out.println("\n");
+                    System.out.println("Потребуется дней: 1");
+                } else System.out.println("Потребуется дней: 2");
+            } else System.out.println("Потребуется дней: 3");
+        } else System.out.println("Доставка не производится");
+        System.out.println("");
 
-        // Р—Р°РґР°РЅРёРµ 5
+        // Задание 5
+        System.out.println("Задание 5");
         int monthNumber = 13;
         switch (monthNumber) {
             case 1:
             case 2:
             case 12:
-                System.out.println(monthNumber + " РјРµСЃСЏС† - СЌС‚Рѕ Р·РёРјР°.");
+                System.out.println(monthNumber + " месяц - это зима.");
                 break;
             case 3:
             case 4:
             case 5:
-                System.out.println(monthNumber + " РјРµСЃСЏС† - СЌС‚Рѕ РІРµСЃРЅР°.");
+                System.out.println(monthNumber + " месяц - это весна.");
                 break;
             case 6:
             case 7:
             case 8:
-                System.out.println(monthNumber + " РјРµСЃСЏС† - СЌС‚Рѕ Р»РµС‚Рѕ.");
+                System.out.println(monthNumber + " месяц - это лето.");
                 break;
             case 9:
             case 10:
             case 11:
-                System.out.println(monthNumber + " РјРµСЃСЏС† - СЌС‚Рѕ РѕСЃРµРЅСЊ.");
+                System.out.println(monthNumber + " месяц - это осень.");
                 break;
             default:
-                System.out.println(monthNumber + " - С‚Р°РєРѕРіРѕ РјРµСЃСЏС†Р° РЅРµ СЃСѓС‰РµСЃС‚РІСѓРµС‚.");
+                System.out.println(monthNumber + " - такого месяца не существует.");
         }
     }
 }
